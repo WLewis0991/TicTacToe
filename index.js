d899ab589aac57b6cf8c0552b9fa350d812f4916
wlewis0991 <wlewis1990@gmail.com>
2026-01-10 16:30:57 -0800

    Player 1 and 2 name and marker assigned

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-//Start screen functionality
-let player1;
-let player2;
-const startButton = document.querySelector(".startButton")
+//Start screen 
 
 
-startButton.addEventListener("click", e =>{
-    player1 = document.getElementById("player1Name").value;
-    player2 = document.getElementById("player2Name").value;
+const player1Name = document.getElementById("player1Name");
+const player2Name = document.getElementById('player2Name');
+const startButton = document.querySelector(".startButton");
+    
+startButton.addEventListener("click", () => {
+    const p1 = player1Name.value;
+    const p2 = player2Name.value;
+    Game.start (p1, p2);
     document.querySelector(".startContainer").classList.add("hidden");
     document.querySelector(".gameBoardContainer").classList.remove("hidden");
+});
+    
+//Create Player
+
+const CreatePlayer = (name, marker) => {
+    const getName = () => name;
+    const getMarker = () => marker;
+    return { getName, getMarker };
+};
+
+//Create Board
+
+//Game Running
+
+const Game = (() => {
+    let players=[];
+    let currentPlayerIndex = 0;
+    let gameOver = false;
 
 
-    console.log(player1)
-    console.log(player2)
-})
 
+    const start = (p1, p2) => {
+        players = [CreatePlayer(p1, "X"), CreatePlayer(p2, "O")]; 
+        currentPlayerIndex = 0;
+        gameOver = false;
+        console.log(players[1].getMarker())  
+        console.log(players[1].getName())     
+    };
+
+    return {start};  
+    
+    
+})();
+
+
+
+//Win Checks
+
+//Restart Button
